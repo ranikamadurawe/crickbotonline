b3d6615251f8ab3c7124e75cb348e9fd7299d179
ranika <ranikamadurawe@gmail.com>
2018-12-30 17:06:08 +0530

    build

--- src/app/dialogflow.service.ts
+++ src/app/dialogflow.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../environments/environment';
 export class DialogflowService {
 
   private baseURL: string = "https://api.dialogflow.com/v1/query?v=20150910";
-  private token: string = environment.token;
+  private token: string = 'a0a59684870c4fee9bd63bad94c6c4ce ';
 
   constructor(private http: Http){}
 
